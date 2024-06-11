0efa7105ee68f198f24e140d5d9d18d74eefee74
ndyabagye <ndyabagyehenrytusi@gmail.com>
2024-06-11 12:19:54 +0300

    Add new vite react project and set up tailwind css

---
+++ go-chat-app-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-red-500 text-lg text-4xl text-center font-bold">Hello world</h2>
+    </>
+  )
+}
+
+export default App
